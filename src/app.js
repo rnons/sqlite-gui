5ed0f742794b19f7818dee248568880b36b44e76
rnons <remotenonsense@gmail.com>
2015-10-26 12:22:07 +0800

    show tables on database connected

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 import 'reflect-metadata';
 import 'zone.js';
 
-import {Component, bootstrap} from 'angular2/angular2';
+import {
+  CORE_DIRECTIVES,
+  Component,
+  NgZone,
+  bootstrap
+} from 'angular2/angular2';
+import {RouterLink} from 'angular2/router';
+
+const ipc = window.require('ipc');
 
 @Component({
   selector: 'sqlite-gui-app',
-  templateUrl: 'src/app.html'
+  templateUrl: 'src/app.html',
+  directives: [CORE_DIRECTIVES, RouterLink]
 })
 class AppComponent{
+  constructor(zone) {
+    this.tables = [];
+    ipc.on('tables', (tables) => {
+      zone.run(() => {
+        this.tables = tables;
+      });
+    });
+  }
 }
+
+AppComponent.parameters = [[NgZone]];
 
 bootstrap(AppComponent);
