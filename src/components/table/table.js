de4fcf9c025510a6cf26f07eb779606e6988418b
rnons <remotenonsense@gmail.com>
2015-10-26 14:52:16 +0800

    show table contents

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,11 +1,27 @@
-import {Component} from 'angular2/angular2';
+const ipc = window.require('ipc');
+
+import {CORE_DIRECTIVES, Component, NgZone} from 'angular2/angular2';
+import {RouteParams} from 'angular2/router';
 
 
 @Component({
   selector: 'table-component',
-  template: 'table com',
+  templateUrl: 'src/components/table/table.html',
+  directives: [CORE_DIRECTIVES]
 })
 export class TableCom {
-  constructor() {
+  constructor(params, zone) {
+    this.name = params.get('name');
+    this.rows = [];
+    this.keys = [];
+    ipc.send('get-table', this.name);
+    ipc.on('table', (data) => {
+      zone.run(() => {
+        this.rows = data;
+        this.keys = Object.keys(this.rows[0]);
+      });
+    })
   }
 }
+
+TableCom.parameters = [[RouteParams], [NgZone]];
