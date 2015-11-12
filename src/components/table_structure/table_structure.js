504788ea3a7c641de89cd5fd30e683f028aada5a
rnons <remotenonsense@gmail.com>
2015-11-12 15:46:04 +0800

    TableStructureCmp works

--- src/components/table_structure/table_structure.js
+++ src/components/table_structure/table_structure.js
@@ -1,12 +1,25 @@
 import {CORE_DIRECTIVES, Component} from 'angular2/angular2';
 
+import {Table} from '../../services/table';
+
 @Component({
   selector: 'table-structure-component',
   templateUrl: 'src/components/table_structure/structure.html',
   directives: [CORE_DIRECTIVES]
 })
 export class TableStructureCmp {
-  constructor() {
+  constructor(table) {
+    this.table = table;
+    this.keys = ['cid', 'name', 'type', 'notnull', 'dflt_value', 'pk'];
+    this.keyNames = {
+      cid: 'Column ID',
+      name: 'Name',
+      type: 'Type',
+      notnull: 'Not Null',
+      dflt_value: 'Default Value',
+      pk: 'Primary Key'
+    };
   }
 }
 
+TableStructureCmp.parameters = [[Table]];
