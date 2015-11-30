39d56da5886f1a3d69e2ac8ae53a5ceecc075300
rnons <remotenonsense@gmail.com>
2015-11-30 15:29:49 +0800

    add GridTable component

---
+++ src/components/grid_table/grid_table.js
@@ -0,0 +1,22 @@
+import {NgFor, Component, ElementRef} from 'angular2/angular2';
+
+@Component({
+  selector: 'grid-table',
+  templateUrl: 'src/components/grid_table/grid_table.html',
+  directives: [NgFor],
+  properties: ['fields', 'rows']
+})
+export class GridTable {
+  constructor(elementRef) {
+    this.$el = elementRef.nativeElement;
+    this.$hidden = this.$el.querySelector('.js-hidden-table');
+  }
+
+  afterViewInit() {
+    const contentWidth = this.$el.getBoundingClientRect().width;
+    const headerWidth = this.$hidden.getBoundingClientRect().width;
+    this.tableWidth = Math.max(contentWidth, headerWidth);
+  }
+}
+
+GridTable.parameters = [[ElementRef]];
